c12892334dc018566c8bb46ab31164bdbbe4a1ec
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-05-15 02:11:49 +0100

    Test: creation of a Librarian passed

---
+++ Librarian/Librarian.js
@@ -0,0 +1,14 @@
+const db = require('../db')
+const Teacher = require('../Teachers/Teacher');
+
+const Librarian = function (name,address,email,password){
+  Teacher.call(this,name,address,email,password);
+  this.cadre = 'Librarian';
+}
+
+Librarian.prototype = Object.create(Teacher.prototype);
+Librarian.prototype.constructor = Teacher;
+
+// console.log(new Librarian('Admin','Library','admin@gmail.com','jyndiqhxqi'));
+
+module.exports = Librarian;
